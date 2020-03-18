8e9352232fb7fcf9d6b42c515a88bf37fd49e7ff
Enrick <enrickdaltro@gmail.com>
2020-03-18 11:33:38 -0300

    connect reactotron

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import React from 'react';
 import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 
+// import '~/config/ReactotronConfig';
+
 import Routes from './routes';
 
 // import { Container } from './styles';
@@ -10,7 +12,7 @@ import Routes from './routes';
 export default function src() {
   return (
     <NavigationContainer>
-      <StatusBar />
+      <StatusBar barStyle="light-content" backgroundColor="#FFF" />
       <Routes />
     </NavigationContainer>
   );
