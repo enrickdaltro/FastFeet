28512adeef11ccd735f803c6c40698b79864154a
Enrick <enrickdaltro@gmail.com>
2020-03-19 01:44:46 -0300

    add: header to dashboard page

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,8 +1,61 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-// import { Container } from './styles';
+import api from '~/services/api';
+
+import {
+  Container,
+  Content,
+  Header,
+  Image,
+  HeaderText,
+  Label,
+  Name,
+  Exit,
+} from './styles';
 
 export default function Dashboard() {
-  return <View />;
+  const profile = useSelector(state => state.deliveryman.profile);
+
+  const [deliverymans, setDeliverymans] = useState(profile);
+
+  // const dispatch = useDispatch();
+
+  useEffect(() => {
+    async function loadDeliveryman() {
+      const response = await api.get(`deliveryman/${profile.id}`);
+
+      setDeliverymans(response.data);
+    }
+    loadDeliveryman();
+  }, [profile.id]);
+
+  return (
+    <Container>
+      <Content>
+        <Header>
+          <Image
+            source={{
+              uri: deliverymans.avatar
+                ? deliverymans.avatar.url
+                : `https://api.adorable.io/avatars/285/${deliverymans.name}.png`,
+            }}
+          />
+
+          <HeaderText>
+            <Label>Bem vindo de volta,</Label>
+            <Name>{deliverymans.name}</Name>
+          </HeaderText>
+
+          <Exit>
+            <TouchableOpacity>
+              <Icon name="exit-to-app" size={24} color="#E74040" />
+            </TouchableOpacity>
+          </Exit>
+        </Header>
+      </Content>
+    </Container>
+  );
 }
--- src/pages/Dashboard/styles.js
+++ src/pages/Dashboard/styles.js
@@ -1,3 +1,35 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+  padding: 0 20px;
+`;
+export const Content = styled.SafeAreaView`
+  flex: 1;
+`;
+export const Header = styled.View`
+  margin-top: 20px;
+  flex-direction: row;
+  align-items: center;
+`;
+export const Image = styled.Image`
+  width: 68px;
+  height: 68px;
+  border-radius: 34px;
+`;
+export const HeaderText = styled.View`
+  margin-left: 20px;
+`;
+export const Label = styled.Text`
+  color: #666;
+`;
+export const Name = styled.Text`
+  margin-top: 3px;
+  font-weight: bold;
+  color: #333;
+  font-size: 24px;
+`;
+
+export const Exit = styled.View`
+  margin-left: 130px;
+`;
