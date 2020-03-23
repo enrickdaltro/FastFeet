0f27fc99783f5e0b96c1c7c5f5b45cda3d18730b
Enrick <enrickdaltro@gmail.com>
2020-03-23 12:18:40 -0300

    add: background component

---
+++ src/components/Background/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Container, Color1, Color2 } from './styles';
+
+export default function Background() {
+  return (
+    <Container>
+      <Color1 />
+      <Color2 />
+    </Container>
+  );
+}
